cf5cf6b7cd223fb28b5bbe68f8aa7cd4b907b018
QESleepy <delano.schild98@gmail.com>
2024-05-22 23:04:31 +0200

    Added FishNet support for UltimateXR!

---
+++ Editor/Sdks/Networking/UxrSdkLocatorFishNet.cs
@@ -0,0 +1,107 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="UxrSdkLocatorFishNet.cs" company="VRMADA">
+//   Copyright (c) VRMADA, All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+using System;
+using UltimateXR.Core;
+using UnityEditor;
+using UnityEngine;
+
+namespace UltimateXR.Editor.Sdks.Networking
+{
+    /// <summary>
+    ///     SDK Locator for the FishNet SDK.
+    /// </summary>
+    public sealed class UxrSdkLocatorFishNet : UxrSdkLocator
+    {
+        #region Public Overrides UxrSdkLocator
+
+        /// <inheritdoc />
+        public override SupportType Support => SupportType.Networking;
+
+        /// <inheritdoc />
+        public override string Name => UxrConstants.SdkFishNet;
+
+        /// <inheritdoc />
+        public override string MinimumUnityVersion => "2020.3";
+
+        /// <inheritdoc />
+        public override string[] AvailableSymbols
+        {
+            get
+            {
+                if (CurrentState == State.Available)
+                {
+                    if (CurrentVersion == 0)
+                    {
+                        return new[] { "ULTIMATEXR_USE_FISHNET_SDK" };
+                    }
+                }
+
+                return Array.Empty<string>();
+            }
+        }
+
+        /// <inheritdoc />
+        public override string[] AllSymbols
+        {
+            get { return new[] { "ULTIMATEXR_USE_FISHNET_SDK" }; }
+        }
+
+        /// <inheritdoc />
+        public override bool CanBeUpdated => true;
+
+        /// <inheritdoc />
+        public override void TryLocate()
+        {
+            CurrentState = State.NotInstalled;
+
+            if (IsTypeInAssemblies("FishNet.Managing.NetworkManager"))
+            {
+                CurrentVersion = 0;
+                CurrentState   = State.Available;
+            }
+        }
+
+        /// <inheritdoc />
+        public override void TryGet()
+        {
+            Application.OpenURL("https://assetstore.unity.com/packages/tools/network/fish-net-networking-evolved-207815");
+        }
+
+        /// <inheritdoc />
+        public override void TryUpdate()
+        {
+            TryGet();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        ///     Auto-registers the locator each time Unity is launched or the project folder is updated.
+        /// </summary>
+        [InitializeOnLoadMethod]
+        public static void RegisterLocator()
+        {
+            UxrSdkManager.RegisterLocator(new UxrSdkLocatorFishNet());
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        ///     Allows to remove dependencies from the project in case the user removed SDK folders manually.
+        /// </summary>
+        [MenuItem(UxrConstants.Editor.MenuPathSdksNetworking + "Remove Symbols for FishNet", priority = UxrConstants.Editor.PriorityMenuPathSdksNetworking)]
+        private static void RemoveSymbols()
+        {
+            UxrSdkManager.RemoveSymbols(new UxrSdkLocatorFishNet());
+        }
+
+        #endregion
+    }
+}
---
+++ Runtime/Scripts/Networking/Integrations/Net/FishNet/UxrFishNetNetwork.cs
@@ -0,0 +1,417 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="UxrFishNetNetwork.cs" company="VRMADA">
+//   Copyright (c) VRMADA, All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+using System.Collections.Generic;
+using UltimateXR.Avatar;
+using UltimateXR.Core;
+using UnityEngine;
+#if ULTIMATEXR_USE_FISHNET_SDK && UNITY_EDITOR
+using UnityEditor;
+#endif
+#if ULTIMATEXR_USE_FISHNET_SDK
+using System.Linq;
+using FishNet.Component.Spawning;
+using FishNet.Component.Transforming;
+using FishNet.Managing;
+using FishNet.Managing.Object;
+using FishNet.Managing.Observing;
+using FishNet.Object;
+using UltimateXR.Extensions.System.Collections;
+using UltimateXR.Extensions.Unity;
+using UltimateXR.Manipulation;
+#endif
+
+#pragma warning disable 414 // Disable warnings due to unused values
+
+namespace UltimateXR.Networking.Integrations.Net.FishNet
+{
+    /// <summary>
+    ///     Implementation of networking support using FishNet.
+    /// </summary>
+    public class UxrFishNetNetwork : UxrNetworkImplementation
+    {
+        #region Inspector Properties/Serialized Fields
+
+        [Tooltip("Show a UI during play mode with connection options to quickly prototype networking functionality")] [SerializeField] private bool _usePrototypingUI = true;
+
+        #endregion
+
+        #region Public Overrides UxrNetworkImplementation
+
+        /// <inheritdoc />
+        public override string SdkName => UxrConstants.SdkFishNet;
+
+        /// <inheritdoc />
+        public override bool IsServer
+        {
+            get
+            {
+#if ULTIMATEXR_USE_FISHNET_SDK
+                return _networkManager != null && _networkManager.IsServer; 
+#else
+                return false;
+#endif
+            }
+        }
+
+        /// <inheritdoc />
+        public override bool IsClient
+        {
+            get
+            {
+#if ULTIMATEXR_USE_FISHNET_SDK
+                return _networkManager != null && _networkManager.IsClient;
+#else
+                return false;
+#endif
+            }
+        }
+
+        /// <inheritdoc />
+        public override UxrNetworkCapabilities Capabilities => UxrNetworkCapabilities.NetworkTransform | UxrNetworkCapabilities.NetworkRigidbody;
+
+        /// <inheritdoc />
+        //public override string NetworkRigidbodyWarning => $"{UxrConstants.SdkFishNet} does not use Rigidbodies";
+
+        /// <inheritdoc />
+        public override void SetupGlobal(UxrNetworkManager networkManager, out List<GameObject> newGameObjects, out List<Component> newComponents)
+        {
+            newGameObjects = new List<GameObject>();
+            newComponents  = new List<Component>();
+
+#if ULTIMATEXR_USE_FISHNET_SDK && UNITY_EDITOR
+            GameObject networkManagerGo = new GameObject("FishNetNetworkManager");
+            Undo.RegisterCreatedObjectUndo(networkManagerGo, "Create FishNet Network Manager");
+            networkManagerGo.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
+            networkManagerGo.transform.SetSiblingIndex(networkManager.transform.GetSiblingIndex() + 1);
+
+            NetworkManager  fishNetNetworkManager = networkManagerGo.GetOrAddComponent<NetworkManager>();
+            ObserverManager observerManager       = networkManagerGo.GetOrAddComponent<ObserverManager>();
+            PlayerSpawner   playerSpawner         = networkManagerGo.GetOrAddComponent<PlayerSpawner>();
+
+            // Try to assign DefaultPrefabObjects asset to NetworkManager. It will still throw an unassigned error,
+            // adding the component triggers an internal check before we can assign anything.
+
+            string[] guids = AssetDatabase.FindAssets("t:DefaultPrefabObjects", new[] { "Assets/" });
+
+            if (guids.Any() && fishNetNetworkManager.SpawnablePrefabs == null)
+            {
+                fishNetNetworkManager.SpawnablePrefabs = AssetDatabase.LoadAssetAtPath<DefaultPrefabObjects>(AssetDatabase.GUIDToAssetPath(guids.First()));
+            }
+
+            Undo.RegisterFullObjectHierarchyUndo(networkManager.gameObject, "Setup FishNet NetworkManager");
+
+            newGameObjects.Add(networkManagerGo);
+
+            newComponents.Add(fishNetNetworkManager);
+            newComponents.Add(observerManager);
+            newComponents.Add(playerSpawner);
+
+#endif
+        }
+
+        /// <inheritdoc />
+        public override void SetupAvatar(UxrAvatar avatar, out List<GameObject> newGameObjects, out List<Component> newComponents)
+        {
+            newGameObjects = new List<GameObject>();
+            newComponents  = new List<Component>();
+
+#if ULTIMATEXR_USE_FISHNET_SDK && UNITY_EDITOR
+            if (avatar == null)
+            {
+                return;
+            }
+            NetworkObject avatarNetworkIdentity = avatar.gameObject.GetOrAddComponent<NetworkObject>();
+            newComponents.Add(avatarNetworkIdentity);
+
+            UxrFishNetAvatar fishNetAvatar = avatar.GetOrAddComponent<UxrFishNetAvatar>();
+            newComponents.Add(fishNetAvatar);
+
+            IEnumerable<Behaviour> avatarComponents    = SetupNetworkTransform(avatar.gameObject,                                  true, UxrNetworkTransformFlags.ChildAll);
+            IEnumerable<Behaviour> cameraComponents    = SetupNetworkTransform(avatar.CameraComponent.gameObject,                  true, UxrNetworkTransformFlags.ChildPositionAndRotation);
+            IEnumerable<Behaviour> leftHandComponents  = SetupNetworkTransform(avatar.GetHand(UxrHandSide.Left).Wrist.gameObject,  true, UxrNetworkTransformFlags.ChildTransform);
+            IEnumerable<Behaviour> rightHandComponents = SetupNetworkTransform(avatar.GetHand(UxrHandSide.Right).Wrist.gameObject, true, UxrNetworkTransformFlags.ChildTransform);
+
+            newComponents.AddRange(avatarComponents.ToList().Concat(cameraComponents).Concat(leftHandComponents).Concat(rightHandComponents));
+
+            Undo.RegisterFullObjectHierarchyUndo(avatar.gameObject, "Setup FishNet Avatar");
+#endif
+        }
+
+        /// <inheritdoc />
+        public override void SetupPostProcess(IEnumerable<UxrAvatar> avatarPrefabs)
+        {
+#if ULTIMATEXR_USE_FISHNET_SDK && UNITY_EDITOR
+            
+            // Find the player spawner and assign the first avatar in the list as the spawnable avatar.
+
+            PlayerSpawner fishNetPlayerSpawner = FindObjectOfType<PlayerSpawner>();
+
+            if (fishNetPlayerSpawner != null)
+            {
+                SerializedObject so = new SerializedObject(fishNetPlayerSpawner);
+
+                if (so != null)
+                {
+                    so.Update();
+                    SerializedProperty sp = so.FindProperty("_playerPrefab");
+
+                    if (sp != null && avatarPrefabs.Any() && sp.objectReferenceValue == null)
+                    {
+                        // If there is no avatar assigned and there is an avatar prefab available, assign the first.
+                        sp.objectReferenceValue = avatarPrefabs.First().GetComponent<NetworkObject>();
+                        so.ApplyModifiedProperties();
+                    }
+                }
+            }
+            
+#endif
+        }
+
+        /// <inheritdoc />
+        public override IEnumerable<Behaviour> AddNetworkTransform(GameObject gameObject, bool worldSpace, UxrNetworkTransformFlags networkTransformFlags)
+        {
+#if ULTIMATEXR_USE_FISHNET_SDK && UNITY_EDITOR
+            if (networkTransformFlags.HasFlag(UxrNetworkTransformFlags.ChildTransform) == false)
+            {
+                NetworkObject networkObject = gameObject.GetOrAddComponent<NetworkObject>();
+                yield return networkObject;
+            }
+
+            UxrFishNetworkTransform networkTransform = gameObject.GetOrAddComponent<UxrFishNetworkTransform>();
+            networkTransform.useWorldSpace = worldSpace;
+
+            yield return networkTransform;
+
+#else
+            yield break;
+#endif
+        }
+
+        /// <inheritdoc />
+        public override IEnumerable<Behaviour> AddNetworkRigidbody(GameObject gameObject, bool worldSpace, UxrNetworkRigidbodyFlags networkRigidbodyFlags)
+        {
+            // FishNet does not use NetworkRigidbody.
+            // We can just use the NetworkTransform if the Rigidbody is kinematic.
+            // Source: https://www.reddit.com/r/Unity3D/comments/vr4du5/comment/iettaaw/
+
+            return AddNetworkTransform(gameObject, worldSpace, UxrNetworkTransformFlags.All);
+        }
+
+        /// <inheritdoc />
+        public override void EnableNetworkTransform(GameObject gameObject, bool enable)
+        {
+#if ULTIMATEXR_USE_FISHNET_SDK
+            
+            UxrFishNetworkTransform[] networkTransforms = gameObject.GetComponentsInChildren<UxrFishNetworkTransform>();
+            networkTransforms.ForEach(nt => nt.SetEnabled(enable));
+            
+#endif
+        }
+
+        /// <inheritdoc />
+        public override void EnableNetworkRigidbody(GameObject gameObject, bool enable)
+        {
+            // FishNet does not use NetworkRigidbody.
+            // We can just use the NetworkTransform if the Rigidbody is kinematic.
+            // Source: https://www.reddit.com/r/Unity3D/comments/vr4du5/comment/iettaaw/
+
+            EnableNetworkTransform(gameObject, enable);
+        }
+
+        /// <inheritdoc />
+        public override bool HasAuthority(GameObject gameObject)
+        {
+#if ULTIMATEXR_USE_FISHNET_SDK
+            NetworkObject networkObject = gameObject.GetComponent<NetworkObject>();
+
+            return networkObject != null && networkObject.IsOwner;
+
+#else
+            return false;
+#endif
+        }
+
+        /// <inheritdoc />
+        public override void RequestAuthority(GameObject gameObject)
+        {
+#if ULTIMATEXR_USE_FISHNET_SDK
+            if (gameObject == null)
+            {
+                return;
+            }
+
+            UxrFishNetAvatar fishNetAvatar = UxrAvatar.LocalAvatar.GetComponentInChildren<UxrFishNetAvatar>();
+            NetworkObject    networkObject = gameObject.GetComponent<NetworkObject>();
+
+            if (networkObject != null && fishNetAvatar != null)
+            {
+                fishNetAvatar.RequestAuthority(networkObject);
+            }
+#endif
+        }
+
+        /// <inheritdoc />
+        public override void CheckReassignGrabAuthority(GameObject gameObject)
+        {
+#if ULTIMATEXR_USE_FISHNET_SDK
+
+            UxrGrabbableObject grabbableObject = gameObject.GetComponent<UxrGrabbableObject>();
+            NetworkObject      networkObject   = gameObject.GetComponent<NetworkObject>();
+
+            if (networkObject != null && grabbableObject != null)
+            {
+                UxrAvatar avatarAuthority = UxrAvatar.EnabledComponents.FirstOrDefault(a => a.GetComponent<NetworkObject>() != null && a.GetComponent<NetworkObject>().OwnerId == networkObject.OwnerId);
+
+                if (avatarAuthority == null || !UxrGrabManager.Instance.IsBeingGrabbedBy(grabbableObject, avatarAuthority))
+                {
+                    // No avatar has authority or the avatar that grabbed it doesn't have it anymore. Change authority to first one.
+                    
+                    UxrAvatar firstAvatar = UxrGrabManager.Instance.GetGrabbingHands(grabbableObject).First().Avatar;
+
+                    if (firstAvatar == UxrAvatar.LocalAvatar)
+                    {
+                        UxrNetworkManager.Instance.RequestAuthority(gameObject);
+                    }
+                }
+            }
+#endif
+        }
+
+        /// <inheritdoc />
+        public override bool HasNetworkTransformSyncComponents(GameObject gameObject)
+        {
+#if ULTIMATEXR_USE_FISHNET_SDK
+            return gameObject.GetComponent<UxrFishNetworkTransform>() != null;
+#else
+            return false;
+#endif
+        }
+
+        #endregion
+
+#if ULTIMATEXR_USE_FISHNET_SDK
+
+        #region Unity
+
+        /// <summary>
+        ///     Unity OnGUI call that will draw the prototyping UI if it's enabled.
+        /// </summary>
+        private void OnGUI()
+        {
+            if (!_usePrototypingUI || !NetworkManager.Instances.First())
+            {
+                return;
+            }
+
+            PosY = 0;
+            GUI.Box(new Rect(0, 0, Screen.width, Screen.height), string.Empty);
+
+            GUI.Box(new Rect(0, PosY, ButtonWidth, ButtonHeight), "UltimateXR Fish Networking");
+            PosY += ButtonHeight;
+            if (_networkManager != null)
+            {
+                if (_networkManager.IsServer || _networkManager.IsClient)
+                {
+                    if (_networkManager.IsServerOnly)
+                    {
+                        if (GUI.Button(new Rect(0, PosY, ButtonWidth, ButtonHeight), "Stop Server"))
+                        {
+                            _networkManager.ServerManager.StopConnection(true);
+                        }
+
+                        PosY += ButtonHeight;
+
+                        if (GUI.Button(new Rect(0, PosY, ButtonWidth, ButtonHeight), "Start Client"))
+                        {
+                            _networkManager.ClientManager.StartConnection("localhost", NetworkPort);
+                        }
+                    }
+                    else if (_networkManager.IsClientOnly)
+                    {
+                        if (GUI.Button(new Rect(0, PosY, ButtonWidth, ButtonHeight), "Disconnect Client"))
+                        {
+                            _networkManager.ClientManager.StopConnection();
+                        }
+                    }
+                    else if (_networkManager.IsServer && _networkManager.IsClient)
+                    {
+                        if (GUI.Button(new Rect(0, PosY, ButtonWidth, ButtonHeight), "Stop Server"))
+                        {
+                            _networkManager.ServerManager.StopConnection(true);
+                        }
+
+                        PosY += ButtonHeight;
+
+                        if (GUI.Button(new Rect(0, PosY, ButtonWidth, ButtonHeight), "Stop Client"))
+                        {
+                            _networkManager.ClientManager.StopConnection();
+                        }
+                    }
+                    return;
+                }
+            }
+            else
+            {
+                _networkManager = NetworkManager.Instances.First();
+            }
+
+            GUI.Box(new Rect(0,                PosY, ButtonWidth,     LabelHeight), "Network Address:");
+            GUI.Box(new Rect(ButtonWidth + 10, PosY, ButtonWidth / 2, LabelHeight), "Port:");
+
+            PosY += LabelHeight;
+
+            _networkAddress =  GUI.TextField(new Rect(0,                PosY, ButtonWidth,     LabelHeight), _networkAddress);
+            _networkPort    =  GUI.TextField(new Rect(ButtonWidth + 10, PosY, ButtonWidth / 2, LabelHeight), _networkPort);
+            PosY            += ButtonHeight;
+
+            if (GUI.Button(new Rect(0, PosY, ButtonWidth, ButtonHeight), "Start Host"))
+            {
+                _networkManager.ServerManager.StartConnection(NetworkPort);
+                _networkManager.ClientManager.StartConnection(_networkAddress, NetworkPort);
+            }
+
+            PosY += ButtonHeight;
+
+            if (GUI.Button(new Rect(0, PosY, ButtonWidth, ButtonHeight), "Start Server"))
+            {
+                _networkManager.ServerManager.StartConnection(NetworkPort);
+            }
+
+            PosY += ButtonHeight;
+
+            if (GUI.Button(new Rect(0, PosY, ButtonWidth, ButtonHeight), "Start Client"))
+            {
+                _networkManager.ClientManager.StartConnection(_networkAddress, NetworkPort);
+            }
+        }
+
+        /// <summary>
+        ///     Gets the network port specified by the user through the UI.
+        /// </summary>
+        private ushort NetworkPort => ushort.Parse(_networkPort);
+
+        #endregion
+
+        #region Private Types & Data
+
+        private int PosY { get; set; }
+
+        private const int LabelHeight  = 25;
+        private const int ButtonWidth  = 200;
+        private const int ButtonHeight = 40;
+
+        private NetworkManager _networkManager;
+
+        private string _networkAddress = "localhost";
+        private string _networkPort    = "7777";
+
+        #endregion
+
+#endif
+    }
+}
+
+#pragma warning restore 414
